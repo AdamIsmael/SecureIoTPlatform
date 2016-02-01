148086ea4ed6b4b1b90127b3faae0156b38d033e
Adam <adam_kidd@hotmail.co.uk>
2016-02-01 16:31:28 +0000

    amendments to include server address

--- SendingData/SendingData.ino
+++ SendingData/SendingData.ino
@@ -2,10 +2,15 @@
 #include <Ethernet2.h>
 #include <SPI.h>
 
-byte mac{} = {};
-Ethernet client client;
+byte mac[] = {
+0x90, 0xA2, 0xDA, 0x10, 0x2D,0xD6
+};
+byte server[]={31,170,160,73};
+
+EthernetClient client;
 
 String data;
+int t = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -15,16 +20,21 @@ void setup() {
     Serial.println("fail");
   }
   
+  t=20;
   data="";
+}
 
 void loop() {
   
-  data="hex";
+  data="temp1" + t;
   
-  if(client.connect("www.")){ //server address
+  if(client.connect(server,80)){ //server address port:21
      client.println("POST /add.php HTTP/1.1"); 
-     client.println("HOST: ") //server address
-     client.print("Data: ");
+     client.println("HOST: 31.170.160.73"); //server address
+     client.println("Content-Type: application/x-www-form-urlencoded");
+     client.print("Content-Length: ");
+     client.println(data.length());
+     client.println();
      client.print(data);
    }
    
